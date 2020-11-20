283e7e68fcf315ac6ea2b4b7de13d37c06bb74d7
Shahriyor Shokirov <shahriyor030387@gmail.ru>
2020-11-19 19:41:01 -0800

    + solved problem three

--- problem_three/src/olmhona/Main.java
+++ problem_three/src/olmhona/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String st = "abcdewfjhqz";
+
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) == 'w') {
+                System.out.println("Раньше w");
+                return;
+            } else if (st.charAt(i) == 'x') {
+                System.out.println("Раньше x");
+                return;
+            }
+        }
+        System.out.println("нет w и x");
     }
 }
