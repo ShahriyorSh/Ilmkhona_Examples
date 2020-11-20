f31840aed343ff3aeef94375dde7d225872aa03c
Shahriyor Shokirov <shahriyor030387@gmail.ru>
2020-11-19 18:33:36 -0800

    solved problem 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-            
+
+        String st = "Tah";
+//        String st2 = "Sony";
+        StringBuilder s = new StringBuilder();
+
+        if (st.length() > 5) {
+            System.out.print(st.substring(0, 3) + " ");
+            System.out.print(st.substring(st.length() - 3));
+        } else {
+            for (int i = 0; i < st.length(); i++) {
+                s.append(st.charAt(0));
+            }
+        }
+        System.out.println( s);
     }
+
 }
