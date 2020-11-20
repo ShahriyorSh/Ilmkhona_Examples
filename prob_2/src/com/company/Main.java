1893913ea03efa46dfa9147aed7130dfe13f4c69
Shahriyor Shokirov <shahriyor030387@gmail.ru>
2020-11-19 18:59:07 -0800

    + solved problem 2

--- prob_2/src/com/company/Main.java
+++ prob_2/src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String st = "tajikistan p feel p the friendship";
+        //System.out.println(st.substring(st.length() - 1));
+
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) == st.charAt(st.length() - 1)) {
+                System.out.println(i);
+            }
+        }
     }
 }
